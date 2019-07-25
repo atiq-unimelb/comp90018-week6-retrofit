fa68a0ad253608132161888bc356e35a1375d12c
Chaofan Wang <chaofanqw@gmail.com>
2019-07-25 15:51:22 +1000

    Initialize Retrofit Demonstration - from Chu Luo.

---
+++ app/src/main/java/io/cluo29/github/retrofit2test1/Request_Interface.java
@@ -0,0 +1,22 @@
+package io.cluo29.github.retrofit2test1;
+
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+
+/**
+ * Created by CLUO29 on 13/08/18.
+ */
+
+public interface Request_Interface {
+
+    // put the request string inside annotation
+//    @GET("ajax.php?a=fy&f=auto&t=auto&w={script}")
+    @GET("ajax.php")
+    Call<MainActivity.Translation> getCall(@QueryMap Map<String, String> params);
+
+}
